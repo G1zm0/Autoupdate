a0626efe68f4bfed5a2ebd5760277f89b2d2c599
G1zm0 <klaas_@hotmail.com>
2012-02-21 22:54:01 +0100

    Used correct python; started couchpotato; commented subliminal

--- Autoupdate.sh
+++ Autoupdate.sh
@@ -22,8 +22,8 @@ PATH_TO_HEADPHONES=/usr/local/var/headphones
 PATH_TO_SCRIPTS_HEADPHONES=/var/packages/SickBeard/scripts
 PATH_TO_SUBLIMINAL=/volume1/@appstore/subliminal
 
-GIT_APP=$(which git)	                # Find the git
-PYTHON_APP=$(which python2.6)           # Find the python executable
+GIT_APP=$(which git)	                		# Find the git
+PYTHON_APP=/usr/local/python26/bin/python		# python26 executable
 
 # Step 1 update SickBeard
 
@@ -68,7 +68,7 @@ update_sickbeard;
 # Step 2 update Couchpotato
 
 # Check if Couchpotato is installed correct
-sickbeard_dir_check () {
+cochpotato_dir_check () {
     if [ $PATH_TO_COUCHPOTATO ]; then    
         if [ ! -d $PATH_TO_COUCHPOTATO ]; then
             echo "Couchpotato not installed or no Zebulon Package used!";
@@ -80,11 +80,11 @@ sickbeard_dir_check () {
 }
 
 
-stop_sickbeard () {
+stop_couchpotato () {
 	$PATH_TO_SCRIPTS_COUCHPOTATO/start-stop-status stop;
 }
 
-start_sickbeard () {
+start_couchpotato () {
 	$PATH_TO_SCRIPTS_COUCHPOTATO/start-stop-status start;
 }
 
@@ -104,49 +104,51 @@ update_couchpotato () {
 	start_couchpotato
 }
 
+update_couchpotato
+
 # Step 3 Updating Headphones
 
 # Step 4 Updating subliminal
-cd $PATH_TO_SUBLIMINAL
+#cd $PATH_TO_SUBLIMINAL
 
 
-TMP_FILE="/tmp/.subliminal-upgrade";    # Tmp file variable
+#TMP_FILE="/tmp/.subliminal-upgrade";    # Tmp file variable
 
-echo "======================"
-echo -n `date +%Y-%m-%d\ %H:%M`;
-echo ": Updating Subliminal";
-echo "";
+#echo "======================"
+#echo -n `date +%Y-%m-%d\ %H:%M`;
+#echo ": Updating Subliminal";
+#echo "";
 
 # Eerst testen of het proces nog niet draait.
 # Dit doe ik aan de hand van het bestaan van een tempfile.
-if [ -f $TMP_FILE ]
-    then
-        echo "Er draait al een update, dus niet nog een starten."
-        # Stoppen met verdere update aangezien de file al aanwezig is.
-        exit
-fi
+#if [ -f $TMP_FILE ]
+#    then
+#        echo "Er draait al een update, dus niet nog een starten."
+#        # Stoppen met verdere update aangezien de file al aanwezig is.
+#        exit
+#fi
 
 # Tempfile aanmaken aangezien de update nog niet draait
-touch $TMP_FILE
-wait;
+#touch $TMP_FILE
+#wait;
 
-if $GIT_APP --git-dir="${PATH_TO_SUBLIMINAL}/.git" pull | grep 'files changed';
-    then
-        echo "Update downloaded"
-        echo "Installing new Subliminal version"
-        echo ""
-        $PYTHON_APP ${PATH_TO_SUBLIMINAL}/setup.py install
-        echo ""
-        wait
-        # Tempfile weer opruimen, anders wordt er maar eenmalig ge-update.
-        rm $TMP_FILE
-        echo "Finished!"
-    else
-        rm $TMP_FILE
-        echo "No update available"
-fi
-
-echo ""
-echo -n `date +%Y-%m-%d\ %H:%M`;
-echo ": Updating ended";
-echo "======================";
+#if $GIT_APP --git-dir="${PATH_TO_SUBLIMINAL}/.git" pull | grep 'files changed';
+#    then
+#        echo "Update downloaded"
+#        echo "Installing new Subliminal version"
+#        echo ""
+#        $PYTHON_APP ${PATH_TO_SUBLIMINAL}/setup.py install
+#        echo ""
+#        wait
+#        # Tempfile weer opruimen, anders wordt er maar eenmalig ge-update.
+#        rm $TMP_FILE
+#        echo "Finished!"
+#    else
+#        rm $TMP_FILE
+#        echo "No update available"
+#fi
+#
+#echo ""
+#echo -n `date +%Y-%m-%d\ %H:%M`;
+#echo ": Updating ended";
+#echo "======================";
